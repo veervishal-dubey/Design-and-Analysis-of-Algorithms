7df4a41ca27402ac38af23d346358f4c586140f5
Veer Vishal Dubey <veervishaldubey@gmail.com>
2026-03-10 12:54:46 +0530

    Correct DFS function and input handling
    Fix syntax errors and improve DFS implementation.

--- dfs.c
+++ dfs.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int count = 1;
 
 int stack[100];
@@ -6,34 +7,34 @@ int graph[10][10];
 
 int visited[100];
 
-        i
 void dfs(int start, int n)
 {
- 
-        visited[start]=count;
-        push(start);
+
+        visited[start]=count++;
+        printf("%d ", start);
         for (int i =0; i<n;i++)
         {
-                if(graph[start[i]!=0 && !visited[i])
+                if(graph[start][i]!=0 && !visited[i])
                         dfs(i,n);
         }
 }
- 
+
 int main()
 {
- 
+
 int n;
 
 printf("Enter the number of vertex: ");
-scanf("%d", n);
+scanf("%d",&n);
 printf("Enter the adjacenecy matrix\n");
 for (int i=0; i<n;i++)
 {
         for (int j = 0 ; j<n ; j++)
                 scanf("%d", &graph[i][j]);
 }
- 
-for (int i=0; i:n; i++)
-        printf("%d ", visited[i]);
+
+dfs(0,n);
+
+
 return 0;
 }
